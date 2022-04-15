c50fcbb29e7f6d35bb71fc975cb558a43dea70c2
epmik <steven.kimpe@tothedot.be>
2022-04-15 21:52:38 +0200

    changed name

---
+++ Scrblr.Leaning/Learn027-Training ground.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using Scrblr.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Scrblr.Leaning
+{
+    [Sketch(Name = "Learn027-Training ground")]
+    public class Learn027 : AbstractSketch
+    {
+        public Learn027()
+            : base(600, 600)
+        {
+        }
+
+        private Texture _texture;
+
+        public void Load()
+        {
+            _texture = new Texture("resources/textures/orange-transparent-1024x1024.png");
+
+            Graphics.ClearColor(1f, 1f, 1f, 1.0f);
+
+            Graphics.Enable(EnableFlag.DepthTest);
+        }
+
+        public void UnLoad()
+        {
+            _texture.Dispose();
+        }
+
+        public void Render()
+        {
+            Graphics.Quad();
+        }
+    }
+}
