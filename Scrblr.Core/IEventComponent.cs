ec9a3c758441e114d1e92e255dc37973b670cf9d
epmik <steven.kimpe@tothedot.be>
2022-04-20 22:16:17 +0200

    added

---
+++ Scrblr.Core/IEventComponent.cs
@@ -0,0 +1,43 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Scrblr.Core
+{
+    public interface IEventComponent : IComponent
+    {
+        double ElapsedTime { get; set; }
+
+        KeyboardState KeyboardState { get; set; }
+
+        MouseState MouseState { get; set; }
+
+        void Update(FrameEventArgs a);
+
+        void Resize(ResizeEventArgs a);
+
+        void KeyUp(KeyboardKeyEventArgs a);
+
+        void KeyDown(KeyboardKeyEventArgs a);
+
+        void MouseDown(MouseButtonEventArgs a);
+
+        void MouseUp(MouseButtonEventArgs a);
+
+        void MouseEnter();
+
+        void MouseLeave();
+
+        void MouseMove(MouseMoveEventArgs a);
+
+        void MouseWheel(MouseWheelEventArgs a);
+    }
+}
