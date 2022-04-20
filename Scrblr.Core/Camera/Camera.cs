be150a8a6a02cae4936c3e0268f8069fced9f46a
epmik <steven.kimpe@tothedot.be>
2022-04-20 22:11:30 +0200

    not used

---
+++ Scrblr.Core/Camera/Camera.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Scrblr.Core
+{
+    public static class Camera
+    {
+
+        public static TCamera CreateFromWidthHeight<TCamera>(float width, float height, float near, float far) where TCamera : AbstractCamera, new()
+        {
+            var camera = new TCamera();
+
+            return camera;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fov">field of view in degrees, between 1 and 120</param>
+        /// <param name="near">distance to the near plane, must be a positive number</param>
+        /// <param name="far">distance to the far plane, must be a positive number</param>
+        public static TCamera CreateFromFieldOfView<TCamera>(float fov, float near, float far) where TCamera : AbstractCamera, new()
+        {
+            var camera = new TCamera();
+
+            return camera;
+        }
+    }
+}
