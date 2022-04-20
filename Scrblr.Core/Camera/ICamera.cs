fc0eaa2af7dc9985c926db0ba73739ec4c41e824
epmik <steven.kimpe@tothedot.be>
2022-04-20 22:12:20 +0200

    added ProjectionMode and AspectRatio

--- Scrblr.Core/Camera/ICamera.cs
+++ Scrblr.Core/Camera/ICamera.cs
@@ -1,13 +1,15 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace Scrblr.Core
 {
-    public interface ICamera
+    public interface ICamera : IEventComponent
     {
+        ProjectionMode ProjectionMode { get; set; }
         float Fov { get; }
         float Near { get; }
         float Far { get; }
@@ -15,6 +17,7 @@ namespace Scrblr.Core
         float Right { get; }
         float Top { get; }
         float Bottom { get; }
+        float AspectRatio { get; }
 
         Matrix4 ProjectionMatrix();
 
