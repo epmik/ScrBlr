2ded1812f3f2d1eb8913214a34accc6446638e7e
epmik <steven.kimpe@tothedot.be>
2022-04-20 22:12:53 +0200

    copied to GraphicsState.cs file

--- Scrblr.Core/Graphics/EnableFlag.cs
+++ Scrblr.Core/Graphics/EnableFlag.cs
@@ -3,21 +3,4 @@ using System;
 
 namespace Scrblr.Core
 {
-    public enum EnableFlag
-    {
-        DepthTest = EnableCap.DepthTest,
-        StencilTest = EnableCap.StencilTest,
-        Texture2d = EnableCap.Texture2D,
-        Blending = EnableCap.Blend,
-        MultiSampling = EnableCap.Multisample,
-        //Culling = EnableCap.CullFace,
-
-        Rendering = 1,
-        ClearBuffer,
-        FrontFaceCulling,
-        BackFaceCulling,
-        ClockWiseFace,
-        CounterClockWiseFace,
-        //FrontAndBackFaceCulling,
-    }
 }
