65793beb92e970b13fe8c5ed949381f74a6dfe15
epmik <steven.kimpe@tothedot.be>
2022-04-24 09:03:10 +0200

    save multi sampled frame buffer to normal frame buffer

--- Scrblr.Leaning/Learn023-SaveFrame-GraphicsContext-MultiSample.cs
+++ Scrblr.Leaning/Learn023-SaveFrame-GraphicsContext-MultiSample.cs
@@ -9,91 +9,45 @@ using System.Text;
 namespace Scrblr.Leaning
 {
     [Sketch(Name = "Learn023-SaveFrame-GraphicsContext-MultiSample")]
-    public class Learn023 : AbstractSketch2d20220413
+    public class Learn023 : AbstractSketch
     {
         public Learn023()
             : base(600, 600)
         {
-            LoadAction += Load;
-            UnLoadAction += UnLoad;
-            RenderAction += Render;
-            UpdateAction += Update;
-
             Samples = 8;
         }
-
-        const string vertexShaderSource = @"
-#version 330 core
-
-in vec3 iPosition;  
-in vec4 iColor;
-
-out vec4 ioColor;
-
-uniform mat4 uModelMatrix;
-uniform mat4 uViewMatrix;
-uniform mat4 uProjectionMatrix;
-
-void main(void)
-{
-    gl_Position = vec4(iPosition, 1.0) * uModelMatrix * uViewMatrix * uProjectionMatrix;
-
-	ioColor = iColor;
-}";
-
-        const string fragmentShaderSource = @"
-#version 330 core
-
-in vec4 ioColor;
-
-out vec4 oColor;
-
-void main()
-{
-    oColor = ioColor;
-}";
 
         public void Load()
         {
             Graphics.ClearColor(255, 255, 255);
 
-            Graphics.CurrentShader = new Shader20220413(vertexShaderSource, fragmentShaderSource);
+            //Graphics.CurrentShader = new Shader20220413(vertexShaderSource, fragmentShaderSource);
 
             QueryGraphicsCardCapabilities();
         }
 
-        public void UnLoad()
-        {
-
-        }
-
         public void Render()
         {
-            Graphics.ClearBuffers();
+            Graphics.ClearBuffers(ClearFlag.ColorBuffer);
 
             Graphics.PushMatrix();
             {
-                Rotate(12);
+                Graphics.Rotate(12);
 
-                Translate(0, 0);
+                Graphics.Translate(0, 0, -2);
 
-                Graphics.Fill(Color4.IndianRed);
-
-                Graphics.Rectangle(0, 0, 100, 100);
+                Graphics.Rectangle().Size(100).Color(Color4.IndianRed);
 
                 {
                     Graphics.PushMatrix();
 
-                    Translate(100, 0, -2);
+                    Graphics.Translate(100, 0, -2);
 
-                    Graphics.Fill(30);
-
-                    Graphics.Quad(
-                        50, 50,
-                        50, -50,
+                    Graphics.Quad().Points(
                         -50, 50,
-                        -50, -50
-                        );
+                        -50, -50,
+                         50, 50,
+                         50, -50).Color(Color4.Red);
 
                     Graphics.PopMatrix();
                 }
@@ -101,53 +55,72 @@ void main()
                 {
                     Graphics.PushMatrix();
 
-                    Translate(50, 0, -4);
+                    Graphics.Translate(-100, 0, -2);
 
-                    Graphics.Fill(60);
-
-                    Graphics.Quad(
-                        -50, -50,
-                        50, -50,
+                    Graphics.Quad().Points(
                         -50, 50,
-                        50, 50);
-
-                    Graphics.PopMatrix();
-                }
-
-                {
-                    Graphics.PushMatrix();
-
-                    Translate(0, 0, -6);
-
-                    Graphics.Fill(90);
-
-                    Graphics.Quad(-50, 50, 50, 50, -50, -50, 50, -50);
-
-                    Graphics.PopMatrix();
-                }
-
-                {
-                    Graphics.PushMatrix();
-
-                    Translate(0, -50, -8);
-
-                    Graphics.Fill(120);
-
-                    Graphics.Quad(
-                        50, -50,
                         -50, -50,
-                        50, 50,
-                        -50, 50);
+                         50, 50,
+                         50, -50).Color(Color4.Yellow);
 
                     Graphics.PopMatrix();
                 }
+
+                {
+                    Graphics.PushMatrix();
+
+                    Graphics.Translate(0, 100);
+
+                    Graphics.Quad().Points(
+                        -50, 50,
+                        -50, -50,
+                         50, 50,
+                         50, -50).Color(Color4.Orange);
+
+                    Graphics.PopMatrix();
+                }
+
+                ////{
+                ////    Graphics.PushMatrix();
+
+                ////    Translate(0, -100, -2);
+
+                ////    Graphics.Fill(Color4.Blue);
+
+                ////    Graphics.LineLoop(
+                ////        50, -50, 
+                ////        -50, -50, 
+                ////        50, 50, 
+                ////        -50, 50);
+
+                ////    Graphics.PopMatrix();
+                ////}
+
+                ////{
+                ////    Graphics.PushMatrix();
+
+                ////    Translate(0, -150, -2);
+
+                ////    {
+                ////        var shape = Graphics.CreateGeometry(GeometryType20220413.Lines);
+
+                ////        shape.Vertex(50, -50).Color(Color4.Blue).Weight(20);
+
+                ////        shape.Vertex(-50, -50).Color(255, 255, 255).Weight(10);
+
+                ////        shape.Vertex(50, 50).Color(255, 0, 255).Weight(5);
+
+                ////        shape.Vertex(-50, 50).Color(255, 255, 0).Weight(50);
+                ////    }
+
+                ////    Graphics.PopMatrix();
+                ////}
             }
             Graphics.PopMatrix();
-        }
 
-        public void Update()
-        {
+            Graphics.Translate(155, 200, -2);
 
+            Graphics.Rectangle().Size(100).Color(Color4.LightGoldenrodYellow);
         }
     }
 }
