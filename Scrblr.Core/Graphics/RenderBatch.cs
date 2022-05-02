7f58d5843b76333607a05397de480a732742c404
epmik <steven.kimpe@tothedot.be>
2022-05-02 09:03:30 +0200

    added

---
+++ Scrblr.Core/Graphics/RenderBatch.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Scrblr.Core
+{
+    public class RenderBatch
+    {
+        public Shader Shader;
+
+        public VertexBuffer VertexBuffer;
+
+        public GraphicsState State;
+
+        public Matrix4 ModelMatrix;
+
+        public Matrix4 ProjectionMatrix;
+
+        public Matrix4 ViewMatrix;
+
+        public Vector3 ViewPosition;
+
+        public GeometryType GeometryType;
+
+        public int ElementIndex;
+
+        public int ElementCount;
+
+        public Texture Texture0;
+
+        public Texture Texture1;
+
+        public Texture Texture2;
+
+        public Texture Texture3;
+
+        public VertexFlag VertexFlag;
+    }
+}
