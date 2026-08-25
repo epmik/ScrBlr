c6ed80c0a0ac03bcd178e20d0b4ce19e23ba7fd2
epmik <steven.kimpe@tothedot.be>
2026-08-25 21:28:25 +0200

    Removed some files

--- Scrblr.Rtx/Main.cs
+++ Scrblr.Rtx/Main.cs
@@ -61,7 +61,7 @@
 
             //new Scrblr.Rtx.Chapter141WithProgress().Main(System.IO.Path.Combine(PathRoot, "chapter141-with-progress.png"));
 
-            //await new Scrblr.Rtx.Chapter141WithProgressThreaded().Main(System.IO.Path.Combine(PathRoot, "chapter141-with-progress-threaded.png"));
+            await new Scrblr.Rtx.Chapter141WithProgressThreaded().Main(System.IO.Path.Combine(PathRoot, "chapter141-with-progress-threaded.png"));
 
             await new Scrblr.Rtx.Chapter200MotionBlur().Main(System.IO.Path.Combine(PathRoot, "chapter200-motion-blur.png"));
 
