7af90250630386333ee99ecdcd1e4d1d58f7dbbe
epmik <steven.kimpe@tothedot.be>
2026-08-27 11:07:30 +0200

    BVH by Jacco Bikker

---
+++ Scrbl.JaccoBikker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Scrbl.JaccoBikker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
